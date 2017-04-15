1d4230c3d569dc59607cc24a9dbf4a22410cccae
Itamar Ravid <iravid@bigpanda.io>
2017-04-15 11:45:53 +0300

    Add a package object for easy importing

---
+++ src/main/scala/com/iravid/playjsoncats/package.scala
@@ -0,0 +1,8 @@
+package com.iravid
+
+package object playjsoncats {
+  object implicits extends
+      JsResultInstances with
+      JsObjectInstances with JsArrayInstances with
+      FormatInstances with ReadsInstances with WritesInstances
+}
